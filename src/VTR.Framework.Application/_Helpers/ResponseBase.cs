e16fa18af15f3852616c1b5d327edda88e95ed37
Vitor <vsteves@apoioprodesp.sp.gov.br>
2025-02-04 23:41:23 -0300

    Included: ResponseBase<T>

--- src/VTR.Framework.Application/_Helpers/ResponseBase.cs
+++ src/VTR.Framework.Application/_Helpers/ResponseBase.cs
@@ -1,5 +1,11 @@
 ﻿namespace VTR.Framework.Application.Contracts;
 
+public class ResponseBase<T>(
+    OperationResult operationResult) : ResponseBase(operationResult)
+{
+    public T? Record { get; set; }
+}
+
 public class ResponseBase(OperationResult operationResult)
 {
     public OperationResult OperationResult { get; } = operationResult;
