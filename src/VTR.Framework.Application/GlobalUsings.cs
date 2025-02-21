a66b891b4b75991164851f1b8b7d4a0b83d17160
Vitor <vsteves@apoioprodesp.sp.gov.br>
2025-02-21 10:04:40 -0300

    CryptoAppService

--- src/VTR.Framework.Application/GlobalUsings.cs
+++ src/VTR.Framework.Application/GlobalUsings.cs
@@ -1,5 +1,7 @@
 ﻿global using FluentValidation.Results;
 global using VTR.Framework.Domain;
 global using VTR.Framework.Domain.Contracts;
+global using VTR.Framework.Domain.Email;
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
+global using System.Text;
---
+++ src/VTR.Framework.Application/Services/CryptoAppService.cs
@@ -0,0 +1,58 @@
+﻿using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace VTR.Framework.Application.Services;
+
+public class CryptoAppService : ICryptoAppService
+{
+    public static string? CreateEncrypt(string value)
+    {
+        return new CryptoAppService().Encrypt(value);
+    }
+
+    public string? Encrypt(string? value)
+    {
+        if (value == null)
+            return null;
+
+        var md5Hasher = MD5.Create();
+
+        byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(value));
+
+        var sb = new StringBuilder();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            sb.Append(data[i].ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+
+    public string GenerateRandomNumber(int length)
+    {
+        if (length <= 0)
+        {
+            return string.Empty;
+        }
+
+        var random = new Random();
+
+        string password = random.Next(1, 9).ToString();
+
+        while (password.Length < length)
+        {
+            password = $"{password}{random.Next(0, 9)}";
+        }
+
+        return password;
+    }
+
+    public string HideCharactersEmail(string email)
+    {
+        string regex = @"(.{2}).+@.+(.{2}(?:\..{2,3}){1,2})";
+        string replace = "$1*@*$2";
+
+        return Regex.Replace(email, regex, replace);
+    }
+}
